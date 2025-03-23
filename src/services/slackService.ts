b3e6ed40b6d4e869770b7c485a5befc2d9ddc51e
Marina-Conejero <marina@hivemechanics.io>
2025-03-23 15:38:07 +0100

    Update slackService.ts

--- src/services/slackService.ts
+++ src/services/slackService.ts
@@ -4,7 +4,7 @@
  */
 
 // Using the provided webhook URL
-const slackWebhookUrl = '';
+const slackWebhookUrl = 'https://hooks.slack.com/services/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';
 
 /**
  * Checks if Slack webhook is configured
